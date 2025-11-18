769fdb2f620ee25f5beb56b317ef9dbf43abbc59
BrunoItikawa10X <brunoitikawa@10xdigital.com.br>
2025-11-18 12:49:25 -0300

    agora é teu, palmeirense roxo

--- .next/dev/server/middleware-build-manifest.js
+++ .next/dev/server/middleware-build-manifest.js
@@ -3,9 +3,22 @@ globalThis.__BUILD_MANIFEST = {
     "/_app": []
   },
   "devFiles": [],
-  "polyfillFiles": [],
+  "polyfillFiles": [
+    "static/chunks/2e76c_next_dist_build_polyfills_polyfill-nomodule.js"
+  ],
   "lowPriorityFiles": [],
-  "rootMainFiles": []
+  "rootMainFiles": [
+    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_f57680da._.js",
+    "static/chunks/2e76c_next_dist_compiled_react-dom_2405f3a8._.js",
+    "static/chunks/2e76c_next_dist_compiled_react-server-dom-turbopack_684698b4._.js",
+    "static/chunks/2e76c_next_dist_compiled_next-devtools_index_e895db3d.js",
+    "static/chunks/2e76c_next_dist_compiled_ee7d69e4._.js",
+    "static/chunks/2e76c_next_dist_client_95811336._.js",
+    "static/chunks/2e76c_next_dist_70c4a58c._.js",
+    "static/chunks/2e76c_@swc_helpers_cjs_7b798936._.js",
+    "static/chunks/Desktop_pididdy-verification-test-api-2_0_a0ff3932._.js",
+    "static/chunks/turbopack-Desktop_pididdy-verification-test-api-2_0_03fb1505._.js"
+  ]
 };
 globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
 "/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
